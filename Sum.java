cfc7bc42598541951f545f42d2070adcc1d9322f
Yuvraj-2060 <yuvrajthakur2060@gmail.com>
2023-03-10 13:16:01 +0530

    My 2nd commit in Sum.java

--- Sum.java
+++ Sum.java
@@ -2,7 +2,8 @@ public class Sum{
     public static void main(String args[]){
         int a=100;
         int b=500;
+        int c=a+b;
 
-        System.out.println(a+b);
+        System.out.println("Sum is : "+c);
     }
 }
