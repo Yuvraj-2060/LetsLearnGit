517fe67deeff6844690f7e168259deff3bfcdac3
Yuvraj-2060 <yuvrajthakur2060@gmail.com>
2023-03-10 16:00:08 +0530

    Adding Multiply.java file

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+public class Multiply {
+    public static void main(String args[]){
+        int a=100;
+        int b=500;
+        int c=a*b;
+
+        System.out.println("Sum is : "+c);
+    }
+}
